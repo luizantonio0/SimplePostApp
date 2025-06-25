358da9d089651413f9c262c6667ccbfc81d4ed09
Luiz <antonio.luiz@ufpr.br>
2025-06-25 11:24:37 -0300

    Add User entity with ID field and JPA annotations

--- src/main/java/com/agora/SimplePostApp/models/User.java
+++ src/main/java/com/agora/SimplePostApp/models/User.java
@@ -1,5 +1,17 @@
 package com.agora.SimplePostApp.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
 
 }
